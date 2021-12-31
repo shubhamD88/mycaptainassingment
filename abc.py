84c82940f3cb39b93002bc007e59b72aa9c91355
shubhamD88 <94123771+shubhamD88@users.noreply.github.com>
2022-01-01 00:22:41 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,13 @@
+r= input ("Enter Radius of Circle : ")
+float(r)
+area = float(3.14) *(float(r)*float(r))
+float(area)
+print("The area of circle with radius " +r+" is")
+print(area)
+b ='abc.py'
+a = input("Enter file name : ")
+if a == b :
+    print("The extension of the file is : Python")
+else:
+    print("Wrong Entry")
+
