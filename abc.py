33d202ae5f2b50e3e5db1cf38c22cd2764967ce7
shubhamD88 <94123771+shubhamD88@users.noreply.github.com>
2022-01-01 00:32:29 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -4,10 +4,4 @@ area = float(3.14) *(float(r)*float(r))
 float(area)
 print("The area of circle with radius " +r+" is")
 print(area)
-b ='abc.py'
-a = input("Enter file name : ")
-if a == b :
-    print("The extension of the file is : Python")
-else:
-    print("Wrong Entry")
 
